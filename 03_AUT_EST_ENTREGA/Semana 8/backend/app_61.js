f5688a946fd34d19dc05a9262acf65ac056af788
Vfernandes1 <vinicius.fernandes@sou.inteli.edu.br>
2022-06-10 18:43:11 -0300

    Update Entrega - Semana 8
    Update Entrega - Semana 8 -
    
    Vinícius Fernandes

---
+++ 03_AUT_EST_ENTREGA/Semana 8/backend/app_61.js
@@ -0,0 +1,49 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const hostname = '127.0.0.1';
+
+const port = 3061;
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const DBPATH = 'dbCurriculodb.db';
+
+app.use(express.static("../frontend"));
+
+
+/* Definição dos endpoints */
+
+/****** CRUD ******************************************************************/
+
+// Retorna todos registros (é o R do CRUD - Read)
+app.get('/users', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  	var sql = 'SELECT * FROM Tbc';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/userinsert', urlencodedParser, (req, res) => {
+
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "INSERT INTO Comentario (Comentario1) VALUES ('" + req.body.comentario + "')";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
+});
---
+++ 03_AUT_EST_ENTREGA/Semana 8/readme.md
@@ -0,0 +1,9 @@
+# Vinícius Fernandes - Exercício Entrega - SEMANA 7
+
+## Descrição
+Endpoints criados, com todos os métodos que foram apresentados em aula.
+
+## Entrega
+03/06/2022
+- Construção de todos os Endpoints para realizar todas as operações CRUD do Banco de Dados (create, read, update, delete)
+- Informações e dados testados no Postman
