fd952eed77afd409495e386c7482e045a099886a
Vinícius Oliveira Fernandes <99264567+Vfernandes1@users.noreply.github.com>
2022-06-21 10:19:55 -0300

    inserindo package e alterando funcionalidades

--- app_61.js
+++ app_61.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-const hostname = '127.0.0.1';
+
 
 const port = 3061;
 const sqlite3 = require('sqlite3').verbose();
@@ -46,4 +46,8 @@ app.post('/userinsert', urlencodedParser, (req, res) => {
 	});
 	db.close(); // Fecha o banco
 	res.end();
+
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server running!`);
 });
